83ff7a51412ad82ad585266b0847df8bba257837
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-09-11 14:51:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,3 +47,17 @@ function getTimesOfDay() {
     setTimeout(showTime, 1000);
 }
 getTimesOfDay();
+
+const name = document.querySelector('.name');
+
+function setLocalStorage() {
+  localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
+  }
+}
+window.addEventListener('load', getLocalStorage);
