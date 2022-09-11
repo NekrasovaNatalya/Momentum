510693dea365cdb526732bee4246d27630515f27
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-09-11 14:51:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,3 +21,29 @@ function showTime() {
     day.textContent = currentDate;
   }
   showDate();
+
+//greeting
+
+const greeting = document.querySelector('.greeting');
+let timeOfDay;
+
+function getTimesOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    
+
+    if (hours/6 <= 1) {
+      timeOfDay = 'night';
+    } else if (hours/6 <= 2 && hours/6 >= 1) {
+      timeOfDay = 'morning';
+    } else if (hours/6 <= 3 && hours/6 >= 2) {
+      timeOfDay = 'afternoon';
+    } else if (hours/6 <= 4 && hours/6 >= 3) {
+      timeOfDay = 'evening';
+    }
+
+    const greetingText = `Good ${timeOfDay},`;
+    greeting.textContent = greetingText;
+    setTimeout(showTime, 1000);
+}
+getTimesOfDay();
