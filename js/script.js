3b40cac621e55da24abf354211669cd4e4b7f116
NN <43994113+NekrasovaNatalya@users.noreply.github.com>
2022-09-12 18:11:03 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,3 +95,31 @@ body.style.backgroundImage = bgUrl;
 
 slideNext.addEventListener('click', getSlideNext);
 slidePrev.addEventListener('click', getSlidePrev);
+
+//weather
+
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector('.temperature');
+const weatherDescription = document.querySelector('.weather-description');
+const city = document.querySelector('.city');
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=a628dcdff6ce0aa348f87ec0c7579546&units=metric`;
+  const res = await fetch(url);
+  const data = await res.json();
+  
+  weatherIcon.className = 'weather-icon owf';
+  weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+  temperature.textContent = `${data.main.temp.toFixed(0)}°C`;
+  weatherDescription.textContent = data.weather[0].description;
+}
+
+function setCity(event) {
+  if (event.code === 'Enter') {
+    getWeather();
+    city.blur();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', getWeather);
+city.addEventListener('keypress', setCity);
